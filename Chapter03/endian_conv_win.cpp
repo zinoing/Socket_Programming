0804aa28449e5c1dc4e43e1d827ba6271489e0fe
zinoing <77779979+zinoing@users.noreply.github.com>
2023-07-24 14:38:06 +0900

    Create  endian_conv_win.cpp

---
+++ Chapter03/endian_conv_win.cpp
@@ -0,0 +1,36 @@
+// endian_conv_win.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <winsock2.h>
+#pragma comment(lib,"WS2_32")
+
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[]) {
+	WSADATA wsaData;
+	unsigned short host_port = 0x1234;
+	unsigned short net_port;
+	unsigned long host_addr = 0x12345678;
+	unsigned long net_addr;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		ErrorHandling("WSAStartup() error!");
+	}
+
+	net_port = htons(host_port);
+	net_addr = htonl(host_addr);
+
+	printf("Host ordered port: %#x \n", host_port);
+	printf("Network ordered port: %#x \n", net_port);
+	printf("Host ordered address: %#x \n", host_addr);
+	printf("Network ordered address: %#x \n", net_addr);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(const char* message) {
+	fputs(message, stderr);
+	fputc('/n', stderr);
+	exit(1);
+}
