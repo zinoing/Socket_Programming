ad77d9a117ea39f0550ba813c6a86a33d170e016
zinoing <77779979+zinoing@users.noreply.github.com>
2023-08-05 23:31:07 +0900

    Create uecho_server_win.c

---
+++ Chapter06/uecho_server_win.c
@@ -0,0 +1,57 @@
+// uecho_server_win.c : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock.h>
+
+#define BUF_SIZE 30
+void ErrorHandling(const char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET servSock;
+	char message[BUF_SIZE];
+	int strLen;
+	SOCKADDR_IN servAdr, clntAdr;
+	int clntAdrSz;
+
+	if (argc != 2) {
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	servSock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (servSock == INVALID_SOCKET)
+		ErrorHandling("socket() error!");
+
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	servAdr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(servSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+		ErrorHandling("bind() error!");
+
+	while (1) {
+		clntAdrSz = sizeof(clntAdr);
+		strLen = recvfrom(servSock, message, BUF_SIZE, 0, (SOCKADDR*)&clntAdr, &clntAdrSz);
+		sendto(servSock, message, strLen, 0, (SOCKADDR*)&clntAdr, sizeof(clntAdr));
+	}
+
+	closesocket(servSock);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
